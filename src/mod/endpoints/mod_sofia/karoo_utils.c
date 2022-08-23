b952362bdfad09426a50d7983014cc06a9f4349e
Joegen Baclor <joegen@ossapp.com>
2022-08-23 10:58:32 +0000

    Add API to change gateway user and password

--- src/mod/endpoints/mod_sofia/karoo_utils.c
+++ src/mod/endpoints/mod_sofia/karoo_utils.c
@@ -48,11 +48,62 @@ void karoo_del_gateway_glob(sofia_profile_t *profile, const char *glob)
 	switch_mutex_unlock(mod_sofia_globals.hash_mutex);
 }
 
+void karoo_set_gateway_route(sofia_profile_t *profile, const char *gwname, const char *route)
+{
+  sofia_gateway_t *gp = NULL;
+  switch_mutex_lock(mod_sofia_globals.hash_mutex);
+  for (gp = profile->gateways; gp; gp = gp->next) {
+    if (!strcasecmp(gwname, gp->name)) {
+      gp->register_route = switch_core_strdup(gp->pool, route);
+      break;
+    }
+  }
+  switch_mutex_unlock(mod_sofia_globals.hash_mutex);
+}
+
+void karoo_set_gateway_auth_username(sofia_profile_t *profile, const char *gwname, const char *user)
+{
+  sofia_gateway_t *gp = NULL;
+  switch_mutex_lock(mod_sofia_globals.hash_mutex);
+  for (gp = profile->gateways; gp; gp = gp->next) {
+    if (!strcasecmp(gwname, gp->name)) {
+      gp->auth_username = switch_core_strdup(gp->pool, user);
+      break;
+    }
+  }
+  switch_mutex_unlock(mod_sofia_globals.hash_mutex);
+}
+
+void karoo_set_gateway_auth_password(sofia_profile_t *profile, const char *gwname, const char *password)
+{
+  sofia_gateway_t *gp = NULL;
+  switch_mutex_lock(mod_sofia_globals.hash_mutex);
+  for (gp = profile->gateways; gp; gp = gp->next) {
+    if (!strcasecmp(gwname, gp->name)) {
+      gp->register_password = switch_core_strdup(gp->pool, password);
+      break;
+    }
+  }
+  switch_mutex_unlock(mod_sofia_globals.hash_mutex);
+}
+
 switch_bool_t karoo_profile_cmd(sofia_profile_t* profile, int argc, char** argv, switch_stream_handle_t *stream)
 {
-  if (!strcasecmp(argv[1], "killgw_glob")) {
+  if (argc == 3 && !strcasecmp(argv[1], "killgw_glob")) {
 		karoo_del_gateway_glob(profile, argv[2]);
 		stream->write_function(stream, "+OK glob %s marked for deletion.\n", argv[2]);
+    return SWITCH_TRUE;
+  } else if (argc == 4 && !strcasecmp(argv[1], "setgw_route")) {
+		karoo_set_gateway_route(profile, argv[2], argv[3]);
+		stream->write_function(stream, "+OK route %s set for gateway %s.\n", argv[2], argv[3]);
+    return SWITCH_TRUE;
+  } else if (argc == 4 && !strcasecmp(argv[1], "setgw_user")) {
+		karoo_set_gateway_auth_username(profile, argv[2], argv[3]);
+		stream->write_function(stream, "+OK user %s set for gateway %s.\n", argv[2], argv[3]);
+    return SWITCH_TRUE;
+  } else if (argc == 4 && !strcasecmp(argv[1], "setgw_password")) {
+		karoo_set_gateway_auth_password(profile, argv[2], argv[3]);
+		stream->write_function(stream, "+OK password set for gateway %s.\n", argv[3]);
     return SWITCH_TRUE;
   }
   return SWITCH_FALSE;
--- src/mod/endpoints/mod_sofia/karoo_utils.h
+++ src/mod/endpoints/mod_sofia/karoo_utils.h
@@ -6,5 +6,8 @@
 switch_bool_t karoo_glob_match(const char* wild, const char* str);
 switch_bool_t karoo_profile_cmd(sofia_profile_t* profile, int argc, char** argv, switch_stream_handle_t *stream);
 void karoo_del_gateway_glob(sofia_profile_t *profile, const char *glob);
+void karoo_set_gateway_route(sofia_profile_t *profile, const char *gwname, const char *route);
+void karoo_set_gateway_auth_username(sofia_profile_t *profile, const char *gwname, const char *user);
+void karoo_set_gateway_auth_password(sofia_profile_t *profile, const char *gwname, const char *password);
 
 #endif /* KAROO_UTILS_H */
