7c9df8058394fcc10ea74c8ed3ec93f9f428cbe0
Joegen Baclor <joegen@baclor.com>
2024-02-13 09:50:44 +0800

    sofia_reg: Allow application to skip gateway checking.  Currently used for lazy gateway configuration reload.

--- src/include/switch_global_funcs.h
+++ src/include/switch_global_funcs.h
@@ -8,12 +8,14 @@ typedef char* (*switch_get_outbound_proxy_fun)(const char* user, const char* rea
 typedef char* (*switch_get_user_agent_fun)(const char* realm);
 typedef char* (*switch_dialplan_fetch_fun)(const char *section, const char *tag_name, const char *key_name, const char *key_value, const char *paramsJson);
 typedef int (*switch_is_user_existing_fun)(const char* user, const char* realm);
+typedef int (*sofia_reg_check_gateway_fun)(void *profile, time_t now);
 struct switch_global_functions
 {
 	switch_get_outbound_proxy_fun switch_get_outbound_proxy;
 	switch_get_user_agent_fun switch_get_user_agent;
 	switch_dialplan_fetch_fun switch_dialplan_fetch;
 	switch_is_user_existing_fun switch_is_user_existing;
+	sofia_reg_check_gateway_fun sofia_reg_check_gateway;
 };
 
 typedef struct switch_global_functions switch_global_functions;
